4c2db4473d1011ca5be9fdb463a72fb9b3d89979
ShaunFlood <shaunflood@hotmail.co.uk>
2023-09-26 21:52:52 +0100

    Added Players, Teams in a different output to touchdowns

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,4 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
 
 namespace WebScraper
 {
@@ -6,7 +8,55 @@ namespace WebScraper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is just a setup");
+            string url = "https://www.espn.co.uk/nfl/stats/player/_/view/scoring/table/scoring/sort/totalTouchdowns/dir/desc";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var touchdownTable = htmlDocument.DocumentNode.SelectNodes("//tr[@class='Table__TR Table__TR--sm Table__even']");
+            var playerElements = htmlDocument.DocumentNode.SelectNodes(".//div[@class='athleteCell__flag flex items-start mr7']");
+            
+            if (touchdownTable != null)
+            {
+                foreach (var row in touchdownTable)
+                {
+                    var columns = row.SelectNodes(".//td[@class='Table__TD']");
+
+                    if (columns != null && columns.Count >= 5)
+                    {
+                        var touchdowns = columns[4].InnerText.Trim();
+                        Console.WriteLine($"Touchdowns: {touchdowns} ");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Table not found.");
+            }
+
+
+            if (playerElements != null)
+            {
+                foreach (var row in playerElements)
+                {
+                    var name = row.SelectSingleNode(".//a[@class='AnchorLink']");
+                    var team = row.SelectSingleNode(".//span[@class='pl2 ns10 athleteCell__teamAbbrev']");
+                    if (name != null || team != null)
+                    {
+                        var players = name.InnerText.Trim();
+                        var playersTeams = team.InnerText.Trim();
+                        Console.WriteLine($"Team: {playersTeams}, Player: {players}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Table not found.");
+            }
+   
+                
+            
         }
     }
 }
